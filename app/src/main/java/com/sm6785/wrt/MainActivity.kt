1e94d90acef422f1085faf21d8ac5d8184324536
Samuele Mattiuzzo <samuele.mattiuzzo@madetech.com>
2023-10-22 19:28:24 +0100

    Rounds counting; from Long to Float

--- app/src/main/java/com/sm6785/wrt/MainActivity.kt
+++ app/src/main/java/com/sm6785/wrt/MainActivity.kt
@@ -12,15 +12,16 @@ import kotlin.math.roundToLong
 
 class MainActivity : ComponentActivity() {
 
-    private var seconds: Long = 0L
+    private var seconds: Float = 0.0f
     private var isRunning: Boolean = false
     private var isCountdown: Boolean = false
+    private var rounds: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState != null) {
-            seconds = savedInstanceState.getLong("seconds")
+            seconds = savedInstanceState.getFloat("seconds")
             isRunning = savedInstanceState.getBoolean("running")
             isCountdown = savedInstanceState.getBoolean("isCountdown")
         }
@@ -29,20 +30,24 @@ class MainActivity : ComponentActivity() {
 
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
         super.onSaveInstanceState(savedInstanceState)
-        savedInstanceState.putLong("seconds", seconds)
+        savedInstanceState.putFloat("seconds", seconds)
         savedInstanceState.putBoolean("running", isRunning)
         savedInstanceState.putBoolean("isCountdown", isCountdown)
     }
 
 
     fun onClickStart(view: View) {
-        seconds = 0L
+        seconds = 0.0f
+        rounds++
+        updateRoundsView()
         isRunning = true
         isCountdown = !isRunning
     }
 
     fun onClickStop(view: View) {
-        seconds = 0L
+        seconds = 0.0f
+        rounds = 0
+        updateRoundsView()
         isRunning = false
         isCountdown = false
     }
@@ -50,25 +55,25 @@ class MainActivity : ComponentActivity() {
     fun onClickRestDouble(view: View) {
         isRunning = false
         isCountdown = !isRunning
-        seconds *= 2L
+        seconds *= 2.0f
     }
 
     fun onClickRestOneHalf(view: View) {
         isRunning = false
         isCountdown = !isRunning
-        seconds *= 1.5f.roundToLong()
+        seconds *= 1.5f
     }
 
     fun onClickRestOne(view: View) {
         isRunning = false
         isCountdown = !isRunning
-        seconds *= 1L
+        seconds *= 1.0f
     }
 
     private fun secondsToString(): String {
-        val hours = seconds / 3600
-        val minutes = (seconds % 3600) / 60
-        val secs = seconds % 60
+        val hours = (seconds / 3600f).toInt()
+        val minutes = ((seconds % 3600f) / 60f).toInt()
+        val secs = (seconds % 60f).toInt()
         return String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs)
     }
 
@@ -77,20 +82,26 @@ class MainActivity : ComponentActivity() {
         timeView.text = secondsToString()
     }
 
+    private fun updateRoundsView() {
+        val roundsView: TextView = findViewById(R.id.rounds_view)
+        roundsView.text = "Round " + rounds.toString()
+    }
+
     private fun timer() {
         val handler = Handler()
         handler.post(object : Runnable {
             override fun run() {
+                updateTimeView()
                 if (isRunning && !isCountdown) {
                     seconds++
                 }
                 else if (!isRunning && isCountdown && seconds>0L) {
                     seconds--
                 }
-                else if (seconds == 0L) {
+                else if (seconds == 0.0f) {
                     isCountdown = false
                 }
-                updateTimeView()
+
                 handler.postDelayed(this, 1_000)
             }
         })
